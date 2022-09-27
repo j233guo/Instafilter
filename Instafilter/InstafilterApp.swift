ed3611879486a29e7e707f77ea676f848326cf52
Jiaming Guo <j23guo@gmail.com>
2022-09-26 22:44:07 -0400

    Initial Commit

---
+++ Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Jiaming Guo on 2022-09-26.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
