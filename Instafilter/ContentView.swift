02473bfe30f8260350e951d9bed5039aeb3ac7dd
Jiaming Guo <j23guo@gmail.com>
2022-10-06 01:02:54 -0400

    constructed main view skeleton

--- Instafilter/ContentView.swift
+++ Instafilter/ContentView.swift
@@ -11,35 +11,44 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var image: Image?
-    @State private var showingImagePicker = false
-    @State private var inputImage: UIImage?
+    @State private var filterIntensity = 0.5
     
-    func loadImage() {
-        guard let inputImage = inputImage else { return }
-        image = Image(uiImage: inputImage)
-    }
+    func save() { }
     
     var body: some View {
-        VStack {
-            image?
-                .resizable()
-                .scaledToFit()
-            Button("Select Image") {
-                showingImagePicker = true
+        NavigationView {
+            VStack {
+                ZStack {
+                    Rectangle()
+                        .fill(.secondary)
+                    Text("Tap to select a picture")
+                        .foregroundColor(.white)
+                        .font(.headline)
+                    image?
+                        .resizable()
+                        .scaledToFit()
+                }
+                .onTapGesture {
+                    // select an image
+                }
+                
+                HStack {
+                    Text("Intensity")
+                    Slider(value: $filterIntensity)
+                }
+                .padding(.vertical)
+                
+                HStack {
+                    Button("Change Filter") {
+                        // change filter
+                    }
+                    Spacer()
+                    Button("Save", action: save)
+                }
             }
-            Button("Save Image") {
-                guard let inputImage = inputImage else { return }
-                let imageSaver = ImageSaver()
-                imageSaver.writeToPhotoAlbum(image: inputImage)
-            }
+            .padding([.horizontal, .bottom])
+            .navigationTitle("Instafilter")
         }
-        .padding()
-        .sheet(isPresented: $showingImagePicker, content: {
-            ImagePicker(image: $inputImage)
-        })
-        .onChange(of: inputImage, perform: { _ in
-            loadImage()
-        })
     }
 }
 
