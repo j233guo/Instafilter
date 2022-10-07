f7a5721884efb2791b6dfff3daec71e8db00ffa4
Jiaming Guo <j23guo@gmail.com>
2022-10-06 20:21:17 -0400

    supported adjusting filter intensity

--- Instafilter/ContentView.swift
+++ Instafilter/ContentView.swift
@@ -14,10 +14,24 @@ struct ContentView: View {
     @State private var filterIntensity = 0.5
     @State private var showingImagePicker = false
     @State private var inputImage: UIImage?
+    @State private var currentFilter = CIFilter.sepiaTone()
+    
+    let context = CIContext()
     
     func loadImage() {
         guard let inputImage = inputImage else { return }
-        image = Image(uiImage: inputImage)
+        let beginImage = CIImage(image: inputImage)
+        currentFilter.setValue(beginImage, forKey: kCIInputImageKey)
+        applyProcessing()
+    }
+    
+    func applyProcessing() {
+        currentFilter.intensity = Float(filterIntensity)
+        guard let outputImage = currentFilter.outputImage else { return }
+        if let cgimg = context.createCGImage(outputImage, from: outputImage.extent) {
+            let uiImage = UIImage(cgImage: cgimg)
+            image = Image(uiImage: uiImage)
+        }
     }
     
     func save() { }
@@ -42,6 +56,9 @@ struct ContentView: View {
                 HStack {
                     Text("Intensity")
                     Slider(value: $filterIntensity)
+                        .onChange(of: filterIntensity, perform: { _ in
+                            applyProcessing()
+                        })
                 }
                 .padding(.vertical)
                 
